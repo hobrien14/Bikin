c92b6d65f535a77980c20a2d695b30011faf96fb
hobrien14 <S02311763@FRCCinst.ccofc.edu>
2017-04-25 21:07:25 -0600

    Brackets
    made a map to work

--- js/bikin.js
+++ js/bikin.js
@@ -1,6 +1,29 @@
-document.addEventListener('DOMContentLoaded', function() {
-    /*
-    your code will go here
-    */
+document.addEventListener('DOMContentLoaded', function () {
+    var map = L.map("map").setView([39.767482, -105.219832], 10);
+
+    L.esri.basemapLayer("Topographic").addTo(map);
     
+    L.esri.featureLayer({
+        url: 'http://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/Tracks/FeatureServer/0'
+    }).addTo(map);
+
+    L.esri.featureLayer({
+        url: 'http://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/Tracks/FeatureServer/1'
+    }).addTo(map);
+
+    L.esri.featureLayer({
+        url: 'http://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/Tracks/FeatureServer/2'
+    }).addTo(map);
+
+    L.esri.featureLayer({
+        url: 'http://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/Tracks/FeatureServer/3'
+    }).addTo(map);
+
+    L.esri.featureLayer({
+        url: 'http://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/Tracks/FeatureServer/4'
+    }).addTo(map);
+    /*
+        your code will go here
+        */
+
 });
