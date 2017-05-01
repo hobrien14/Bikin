78834e46797e34a6a4f9990ce33a727424fec6b1
David Puckett <david@brightrain.com>
2017-05-01 10:07:30 -0600

    fixes to get map working, add styling
    fix for js missing closing brackets sequence, move title text into html
    and make whole title container float on top of map, style jeffco trails
    by difficulty, cleanup popup function, style first trail layer

--- js/bikin.js
+++ js/bikin.js
@@ -3,9 +3,38 @@ document.addEventListener('DOMContentLoaded', function() {
     var map = L.map("map").setView([39.767482, -105.219832], 10);
 
     L.esri.basemapLayer("Topographic").addTo(map);
-    
+
+    var trails = L.esri.featureLayer({
+        url:"http://maps.jeffco.us/arcgis/rest/services/jMap/OpenSpaceParkTrails/MapServer/3",
+        style: function (feature) {
+            var c;
+            switch (feature.properties.TRAIL_DIFFICULTY_RATING) {
+                case 'Least Difficult':
+                    c = '#a0b27f';
+                    break;
+                case 'More Difficult':
+                    c = '#FFEB00';
+                    break;
+                case 'Most Difficult':
+                    c = '#e70000';
+                    break;
+                default:
+                    c = '#FFEB00';
+                    break;
+                                                              }
+            return { color: c, opacity: 1, weight: 3 }
+        }
+    }).addTo(map);
+
+    trails.bindPopup(function (layer) {
+        return L.Util.template('<p><h2>{TRAIL_NAME}</h2><h5>{TRAIL_DIFFICULTY_RATING}</h5><h5>{USER_TYPE}</h5></p>', layer.feature.properties);
+    });
+
     L.esri.featureLayer({
-        url: 'http://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/Tracks/FeatureServer/0'
+        url: 'http://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/Tracks/FeatureServer/0',
+        style: function (feature) {
+            return { color: '#dfa855', opacity: 1, weight: 3 }
+        }
     }).addTo(map);
 
     L.esri.featureLayer({
@@ -23,15 +52,5 @@ document.addEventListener('DOMContentLoaded', function() {
     L.esri.featureLayer({
         url: 'http://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/Tracks/FeatureServer/4'
     }).addTo(map);
-    
-    var trails = L.esri.featureLayer({url:"http://maps.jeffco.us/arcgis/rest/services/jMap/OpenSpaceParkTrails/MapServer/3"}).addTo(map);
 
-trails.bindPopup(function (layer) {
-  return L.Util.template('<p><h2>{TRAIL_NAME}</h2><h5>{TRAIL_DIFFICULTY_RATING}</h5><h5>{USER_TYPE}</h5></p>', layer.feature.properties);
-
-    function showLatLng(e) {
-  document.getElementById("whatIsThis").innerText =  "Bell Joy Ride is a program that encourages women to get out and mountain bike. The trails shown are the group ride trails that will be happening in the summer of 2017."
-}
-map.on('mousemove', showLatLng);
-    
 });
